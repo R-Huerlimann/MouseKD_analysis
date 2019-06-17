acc545f26da9d167a74de44854ac38324c4a14a5
R-Huerlimann <R-Huerlimann@users.noreply.github.com>
2019-06-17 16:49:05 +1000

    Update 02_denoising.sh

--- 02_denoising.sh
+++ 02_denoising.sh
@@ -3,8 +3,8 @@
 #p-n-threads => how many cores are used. 0 = all available, change to more reasonable number if required
 qiime dada2 denoise-paired \
   --i-demultiplexed-seqs manifestout.qza \
-  --p-trim-left-f 17 \
-  --p-trim-left-r 20 \
+  --p-trim-left-f 20 \
+  --p-trim-left-r 21 \
   --p-trunc-len-f 270 \
   --p-trunc-len-r 230 \
   --p-max-ee 6 \
